c53f105b72f000ae5072920a0c906b43ae9fb8de
Jonathan Rossetto de Fraga <jonathanrossettodefraga@gmail.com>
2025-10-23 15:20:59 -0300

    :rocket: implementada rota raiz / para status da API

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'status'        => 'success',
+        'message'       => 'API de Gerenciamento de Veículos em execução.',
+        'documentation' => 'Consulte a coleção Postman para detalhes dos endpoints.'
+    ]);
 });
